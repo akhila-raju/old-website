827340821b0ead02990edaff1e7569f2495b5df4
Akhila Raju <akhilaraju@berkeley.edu>
2014-10-15 12:48:36 -0700

    center label

--- html/script.js
+++ html/script.js
@@ -96,7 +96,7 @@ d3.csv("pobelter.csv", type, function(error, data) {
 
     chart.append("text")      // text label for the y axis
         .attr("transform", "rotate(-90)")
-        .attr("x",  -height/2-25)
+        .attr("x",  -height/2-85)
         .attr("y",  -30)
         .text("Win Rate (# of Wins / # of Games)");
 
